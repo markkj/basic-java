0a00873de1523aa8f14d8f45a5ccde9047b28806
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-17 14:41:09 +0700

    add myRange with simple

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Employee someEmployee = new Employee();
-        String result = someEmployee.getFullName();
-        System.out.println(result);
+//        Employee someEmployee = new Employee();
+//        String result = someEmployee.getFullName();
+//        System.out.println(result);
+
+        MyRange mr = new MyRange("[1,5]");
+        System.out.println(mr.showResult());;
     }
 }
--- src/MyRange.java
+++ src/MyRange.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyRange {
+    String input;
+
+    public MyRange(String input){
+        this.input = input;
+    }
+    public boolean startWithInclude(){
+        return this.input.startsWith("[");
+    }
+
+    public boolean startWithExclude(){
+        return this.input.startsWith("(");
+    }
+
+    public boolean endWithInclude(){
+        return this.input.endsWith("]");
+    }
+
+    public boolean endWithExclude(){
+        return this.input.endsWith("(");
+    }
+
+    public int getFirstNumber(){
+        return Character.getNumericValue(this.input.charAt(1));
+    }
+
+    public int getSecondNumber(){
+        return Character.getNumericValue(this.input.charAt(3));
+    }
+
+    public String showResult(){
+        int startNumber = this.startWithInclude() ? this.getFirstNumber():this.getFirstNumber()+1;
+        int endNumber = this.endWithInclude() ? this.getSecondNumber()+1:this.getSecondNumber();
+        System.out.println(startNumber);
+        System.out.println(endNumber);
+        List<String> result = new ArrayList<>();
+        for (int i = startNumber; i < endNumber; i++) {
+            result.add(Integer.toString(i));
+        }
+        return String.join(",",result);
+    }
 
 }
