d2bf60650ea090840c9dc843b740f4335f400b89
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-17 11:37:16 +0700

    add Constructor

--- src/Employee.java
+++ src/Employee.java
@@ -3,7 +3,15 @@ public class Employee {
     private String firstName;
     private String lastName;
 
+    public Employee(){
+        this("??","??");
+    }
+
+    public Employee(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
     public String getFullName(){
-        return firstName + "-" + lastName;
+        return firstName + " - " + lastName;
     }
 }
