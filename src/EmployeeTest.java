6e83749480d9c7c109e301e37b1e0f1b5f056d10
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-17 13:38:02 +0700

    Add EmployeeTest

--- src/EmployeeTest.java
+++ src/EmployeeTest.java
@@ -1,4 +1,21 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EmployeeTest {
-  
+
+    @Test
+    @DisplayName("Test Create Employee with set value in constructor should return 'Mark - Bk' ")
+    public void testCreateEmployeeWithSetConstructor(){
+        Employee emp = new Employee("Mark","Bk");
+        assertEquals("Mark - Bk",emp.getFullName());
+    }
+
+    @Test
+    @DisplayName("Test Create Employee with default constructor should return ' - ' ")
+    public void testCreateEmployeeWithDefaultConstructor(){
+        Employee emp = new Employee();
+        assertEquals(" - ",emp.getFullName());
+    }
 }
