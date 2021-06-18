3883fa2a107a1a04ad6aa1d9daafe33ccc5f2b0a
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-18 11:59:45 +0700

    override toString() and equals()

---
+++ hello-spring/src/main/java/com/example/hellospring/DemoOOP.java
@@ -0,0 +1,22 @@
+package com.example.hellospring;
+
+public class DemoOOP {
+
+    public static void main(String[] args) {
+
+
+
+        Employee employee = new Employee();
+        Employee employee2 = new Employee();
+
+
+
+
+        if(employee.equals(employee2)){
+            System.out.println("1 == 2");
+        }
+
+        System.out.println(employee);
+
+    }
+}
