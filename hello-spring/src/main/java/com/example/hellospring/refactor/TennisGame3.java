dbed0ab9659a419d5d56b3070f49101e2921e878
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-20 16:03:12 +0700

    Refector p1,p2 => playerOnePoint, playerTwoPoint

--- hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
@@ -2,8 +2,8 @@ package com.example.hellospring.refactor;
 
 public class TennisGame3 {
 
-    private int p2;
-    private int p1;
+    private int playerTwoPoint;
+    private int playerOnePoint;
     private String playerOneName;
     private String playerTwoName;
 
@@ -14,23 +14,23 @@ public class TennisGame3 {
 
     public String getScore() {
         String s;
-        if (p1 < 4 && p2 < 4 && !(p1 + p2 == 6)) {
+        if (playerOnePoint < 4 && playerTwoPoint < 4 && !(playerOnePoint + playerTwoPoint == 6)) {
             String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-            s = p[p1];
-            return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+            s = p[playerOnePoint];
+            return (playerOnePoint == playerTwoPoint) ? s + "-All" : s + "-" + p[playerTwoPoint];
         } else {
-            if (p1 == p2)
+            if (playerOnePoint == playerTwoPoint)
                 return "Deuce";
-            s = p1 > p2 ? playerOneName : playerTwoName;
-            return ((p1-p2)*(p1-p2) == 1) ? "Advantage " + s : "Win for " + s;
+            s = playerOnePoint > playerTwoPoint ? playerOneName : playerTwoName;
+            return ((playerOnePoint - playerTwoPoint)*(playerOnePoint - playerTwoPoint) == 1) ? "Advantage " + s : "Win for " + s;
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName == "player1")
-            this.p1 += 1;
+            this.playerOnePoint += 1;
         else
-            this.p2 += 1;
+            this.playerTwoPoint += 1;
 
     }
 
