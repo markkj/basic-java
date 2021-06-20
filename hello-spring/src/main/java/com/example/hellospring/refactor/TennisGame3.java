4b5a3acc69a6c60b6b1cec53ceb92730e1b5c28f
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-20 17:27:51 +0700

    Split Referee Call

--- hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
+++ hello-spring/src/main/java/com/example/hellospring/refactor/TennisGame3.java
@@ -4,7 +4,7 @@ public class TennisGame3 {
 
     private Player playerOne;
     private Player playerTwo;
-
+    private String nameOfLeading;
 
     public TennisGame3(String playerOneName, String playerTwoName) {
         this.playerOne = new Player(playerOneName);
@@ -26,34 +26,61 @@ public class TennisGame3 {
         }
     }
 
+    public String printRefereeCallAll(){
+        return checkPointState(playerOne.getPoint()) + "-All";
+    }
+
+    public String printRefereeCallNormal(){
+        return checkPointState(playerOne.getPoint()) + "-" + checkPointState(playerTwo.getPoint());
+    }
+
+    public String printRefereeCallDeuce(){
+        return "Deuce";
+    }
+
+    public String printRefereeCallAdvantage(){
+        return "Advantage " + nameOfLeading;
+    }
+
+    public String printRefereeCallWin(){
+        return "Win for " + nameOfLeading;
+    }
     public String getScore() {
 
         boolean isBothPlayerPointLessThan4 = playerOne.getPoint() < 4 && playerTwo.getPoint() < 4;
-        boolean isBothPlayerSumPointNotEqual6 = !(playerOne.getPoint() + playerTwo.getPoint() == 6);
-        boolean isPlayerDeuce = playerOne.getPoint() == playerTwo.getPoint();
+        boolean isBothPlayerSumPointNotEqual6 = (playerOne.getPoint() + playerTwo.getPoint() != 6);
+        boolean isPlayerPointEqual = playerOne.getPoint() == playerTwo.getPoint();
 
         if (isBothPlayerPointLessThan4 && isBothPlayerSumPointNotEqual6) {
 
-            if(isPlayerDeuce){
-                return checkPointState(playerOne.getPoint()) + "-All";
+            if(isPlayerPointEqual){
+                return printRefereeCallAll();
             }
             else{
-                return checkPointState(playerOne.getPoint()) + "-" + checkPointState(playerTwo.getPoint());
+                return printRefereeCallNormal();
             }
-        } else {
-            if (isPlayerDeuce)
-                return "Deuce";
-            String nameOfLeading = playerOne.getPoint() > playerTwo.getPoint() ? playerOne.getName() : playerTwo.getName();
 
-            return ((playerOne.getPoint() - playerTwo.getPoint())*(playerOne.getPoint() - playerTwo.getPoint()) == 1) ? "Advantage " + nameOfLeading : "Win for " + nameOfLeading;
+        } else {
+            if (isPlayerPointEqual)
+                return printRefereeCallDeuce();
+
+            nameOfLeading = playerOne.getPoint() > playerTwo.getPoint() ? playerOne.getName() : playerTwo.getName();
+            boolean isOnAdvantage = (playerOne.getPoint() - playerTwo.getPoint())*(playerOne.getPoint() - playerTwo.getPoint()) == 1;
+            if(isOnAdvantage){
+                return printRefereeCallAdvantage();
+            }else{
+                return printRefereeCallWin();
+            }
         }
     }
 
     public void wonPoint(String playerName) {
         if (playerName == playerOne.getName())
             this.playerOne.addPoint();
-        else
+        else if(playerName == playerTwo.getName())
             this.playerTwo.addPoint();
+        else
+            throw new RuntimeException("playerName not exits");
 
     }
 
