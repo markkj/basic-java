40bfddb3d5876dc61ed967da722ac75490b0facc
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-21 09:53:49 +0700

    Add Hello-Rest

---
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/HeloRestWithGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.helorestwithgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeloRestWithGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeloRestWithGradleApplication.class, args);
+	}
+
+}
---
+++ helo-rest-with-gradle/src/test/java/com/example/helorestwithgradle/HeloRestWithGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helorestwithgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeloRestWithGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
