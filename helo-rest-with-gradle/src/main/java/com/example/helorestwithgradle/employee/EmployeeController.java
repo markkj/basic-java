4a905a79b1cbefed7b124b1016e94b5274308c2a
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-21 13:42:56 +0700

    ADD RequesParam in EmployeeController

--- helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/employee/EmployeeController.java
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/employee/EmployeeController.java
@@ -1,15 +1,25 @@
 package com.example.helorestwithgradle.employee;
 
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EmployeeController {
 
     @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeByID(@PathVariable String id){
+        int _id = 0;
+        try{
+            _id = Integer.parseInt(id);
+        } catch (NumberFormatException e){
+            //Can't Convert;
+            _id = 0;
+        }
+
+        return new EmployeeResponse(_id,"Khajohnyos","Mark");
+    }
+
+    @GetMapping("/employee")
+    public EmployeeResponse getEmployeeByID2(@RequestParam String id){
         int _id = 0;
         try{
             _id = Integer.parseInt(id);
