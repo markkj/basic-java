9cd8bca45944f88fed013e1fde61c939541ed220
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-21 11:38:22 +0700

    Add EmployeeController

--- helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/employee/EmployeeController.java
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/employee/EmployeeController.java
@@ -1,10 +1,12 @@
 package com.example.helorestwithgradle.employee;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
 
+    @GetMapping("/employee/{id}")
     public EmployeeResponse getEmployeeByID(int id){
         return new EmployeeResponse(id,"Khajohnyos","Mark");
     }
