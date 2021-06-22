21a61ad9c4eab4cefc2e62755e14b34755088072
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-22 12:24:33 +0700

    add controller

--- helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/controller/UserController.java
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/controller/UserController.java
@@ -1,4 +1,21 @@
 package com.example.helorestwithgradle.controller;
 
+import com.example.helorestwithgradle.user.Address;
+import com.example.helorestwithgradle.user.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class UserController {
+
+    @GetMapping("/users")
+    public List<User> getUsers(@RequestParam int limits){
+        List<User> users = new ArrayList<>();
+
+        return users;
+    }
 }
--- helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/user/User.java
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/user/User.java
@@ -1,12 +1,23 @@
 package com.example.helorestwithgradle.user;
 
 public class User{
-	private String website;
+	private int id;
+	private String name;
+	private String username;
+	private String email;
 	private Address address;
 	private String phone;
-	private String name;
+	private String website;
 	private Company company;
-	private int id;
-	private String email;
-	private String username;
+
+	public User(int id, String name, String username, String email, Address address, String phone, String website, Company company) {
+		this.id = id;
+		this.name = name;
+		this.username = username;
+		this.email = email;
+		this.address = address;
+		this.phone = phone;
+		this.website = website;
+		this.company = company;
+	}
 }
