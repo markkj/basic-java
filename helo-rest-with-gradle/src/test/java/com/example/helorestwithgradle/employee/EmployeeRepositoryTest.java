2cc5dd60c2085f6076b577c241664416f07e9563
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-23 10:09:01 +0700

    Testing DataJPA mock

--- helo-rest-with-gradle/src/test/java/com/example/helorestwithgradle/employee/EmployeeRepositoryTest.java
+++ helo-rest-with-gradle/src/test/java/com/example/helorestwithgradle/employee/EmployeeRepositoryTest.java
@@ -3,17 +3,24 @@ package com.example.helorestwithgradle.employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @DataJpaTest
 class EmployeeRepositoryTest {
 
-    @Autowired
+    @MockBean
     private EmployeeRepository repository;
+
 
     @Test
     public void foundWithID1(){
+        Employee expectEmp = new Employee("Mark from DB","Bk from DB");
+        expectEmp.setId(1);
+        when(repository.getById(1)).thenReturn(expectEmp);
+
         Employee employee1 = repository.getById(1);
         assertEquals(1, employee1.getId());
         assertEquals("Mark from DB",employee1.getFirstName());
