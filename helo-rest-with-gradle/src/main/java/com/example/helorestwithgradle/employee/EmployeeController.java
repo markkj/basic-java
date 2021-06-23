8c31c5ed53699140374efe45695b85ffcd4e4754
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-23 10:44:54 +0700

    Try  controller connect to repository

--- helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/employee/EmployeeController.java
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/employee/EmployeeController.java
@@ -2,48 +2,70 @@ package com.example.helorestwithgradle.employee;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Random;
 
 @RestController
 public class EmployeeController {
 
+    @Autowired
+    private EmployeeRepository repository;
 
     @Autowired
     private RandomNumber randomNumberEmployee;
 
     @GetMapping("/employee/{id}")
-    public EmployeeResponse getEmployeeByID(@PathVariable String id) {
+
+    public EmployeeResponse getEmployeeByIDWithPathVariable(@PathVariable String id) {
         int _id = 0;
         try {
             _id = Integer.parseInt(id);
+
         } catch (NumberFormatException e) {
             //Can't Convert;
-            _id = 0;
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Invalid ID", e);
         }
 
-        int number = randomNumberEmployee.nextInt(10);
-        return new EmployeeResponse(_id, "Khajohnyos" + number, "Mark");
+        try {
+            Employee emp = repository.getById(_id);
+            return new EmployeeResponse(emp.getId(), emp.getFirstName(), emp.getLastName());
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "ID not found", e);
+        }
     }
 
     @GetMapping("/employee")
-    public EmployeeResponse getEmployeeByID2(@RequestParam String id) {
+    public EmployeeResponse getEmployeeByIDWithRequestParam(@RequestParam String id) {
         int _id = 0;
         try {
             _id = Integer.parseInt(id);
+
         } catch (NumberFormatException e) {
             //Can't Convert;
-            _id = 0;
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST, "Invalid ID", e);
         }
-        int number = randomNumberEmployee.nextInt(10);
-        return new EmployeeResponse(_id, "Khajohnyos" + number, "Mark");
+
+        try {
+            Employee emp = repository.getById(_id);
+            return new EmployeeResponse(emp.getId(), emp.getFirstName(), emp.getLastName());
+        } catch (EntityNotFoundException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND, "ID not found", e);
+        }
     }
 
     @PostMapping("/employee")
     public EmployeeResponse createEmployee(@RequestBody EmployeeRequest request) {
-        int number = randomNumberEmployee.nextInt(10);
-        return new EmployeeResponse(999, request.getFname() + number, request.getLname());
+        Employee emp = new Employee(request.getFname(),request.getLname());
+        Employee savedEmp = repository.save(emp);
+        return new EmployeeResponse(savedEmp.getId(),savedEmp.getFirstName(),savedEmp.getLastName());
 
     }
 }
