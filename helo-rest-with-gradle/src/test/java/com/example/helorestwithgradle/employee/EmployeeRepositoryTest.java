c2f3e0dc26b35e28dadc064577d8cbbbfb48dc18
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-23 10:26:49 +0700

    Testing connection database not test business logicgit add helo-rest-with-gradlegit add helo-rest-with-gradle

--- helo-rest-with-gradle/src/test/java/com/example/helorestwithgradle/employee/EmployeeRepositoryTest.java
+++ helo-rest-with-gradle/src/test/java/com/example/helorestwithgradle/employee/EmployeeRepositoryTest.java
@@ -11,20 +11,21 @@ import static org.mockito.Mockito.when;
 @DataJpaTest
 class EmployeeRepositoryTest {
 
-    @MockBean
+    @Autowired
     private EmployeeRepository repository;
 
 
     @Test
     public void foundWithID1(){
         Employee expectEmp = new Employee("Mark from DB","Bk from DB");
-        expectEmp.setId(1);
-        when(repository.getById(1)).thenReturn(expectEmp);
+        repository.save(expectEmp);
 
         Employee employee1 = repository.getById(1);
         assertEquals(1, employee1.getId());
         assertEquals("Mark from DB",employee1.getFirstName());
         assertEquals("Bk from DB",employee1.getLastName());
     }
+
+
 
 }
