e7c2f43d8b93fd4f547e45f7203d1962b7c79f7d
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-06-23 14:13:13 +0700

    Add UserModel, Users

---
+++ helo-rest-with-gradle/src/main/java/com/example/helorestwithgradle/user/Users.java
@@ -0,0 +1,15 @@
+package com.example.helorestwithgradle.user;
+
+import java.util.*;
+
+public class Users {
+    private List<UserModel> users;
+
+    public List<UserModel> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<UserModel> users) {
+        this.users = users;
+    }
+}
