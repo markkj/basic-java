6e116a26c05bb442b6ea4eed6e2b87cd7b8f1f9a
Khajohnyos Boonkate <khajohnyos.b@kbtg.tech>
2021-08-02 14:39:46 +0700

    add exception demo

--- maven-demo/src/main/java/Demo.java
+++ maven-demo/src/main/java/Demo.java
@@ -1,4 +1,8 @@
 public class Demo {
-
+    public static void main(String[] args) {
+        ExceptionDemo d = new ExceptionDemo();
+        int id = d.method1("a");
+        System.out.println(id);
+    }
 
 }
